b9aa52474dcec2d9ac0d74e94951e1b33f9451fe
Magdalena Sikorska <madzia.sikorska@gmail.com>
2012-06-05 14:15:25 +0200

    handling correct empty response

--- lib/yammer_api/mash.rb
+++ lib/yammer_api/mash.rb
@@ -5,7 +5,7 @@ module YammerApi
   class Mash < ::Hashie::Mash
 
     def self.from_json(json_string)
-      result_hash = MultiJson.decode(json_string)
+      result_hash = MultiJson.decode(json_string || "{}")
       new(result_hash)
     end
 
